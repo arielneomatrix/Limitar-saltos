aac1dc35f6223f22907970e0e2bb558892698db7
Ariel <ariel.arielv@gmail.com>
2025-09-09 15:01:38 -0400

    Mover plataforma

--- Assets/Scripts/Plataformamovible.cs
+++ Assets/Scripts/Plataformamovible.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Plataformamovible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public Transform pointA;
+   public Transform pointB;
+   public bool goingUp;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 wantedPosition = Vector3.zero;
+
+        if (goingUp)
+            wantedPosition = pointA.position;
+        else
+            wantedPosition = pointB.position;
+
+        Vector3 direction = (wantedPosition - transform.position);
+        transform.position += direction.normalized * Time.deltaTime;
+
+        if (direction.magnitude < 1)
+        {
+            if (goingUp)
+                goingUp = false;
+            else
+                goingUp = true;
+        }
+
+
     }
 }
